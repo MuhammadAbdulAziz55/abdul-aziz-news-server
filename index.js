aa9add34fcf0c1c1e26bbca82e98119e0ff89511
MuhammadAbdulAziz55 <abdulazizfrince5@gmail.com>
2023-02-18 15:50:43 +0600

    add vercel config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 const cors = require("cors");
 app.use(cors());
 app.get("/", (req, res) => {
@@ -10,6 +11,26 @@ app.get("/", (req, res) => {
 
 app.get("/news-categories", (req, res) => {
   res.send(categories);
+});
+
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
 });
 
 app.listen(port, () => {
